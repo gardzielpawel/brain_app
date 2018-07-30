3a7e466352e57f7ee3100e278cdcd6816a9cc659
Paweł Gardziel <gardzielpawel@gmail.com>
2018-07-30 21:35:34 +0200

    secondcommit

--- brainscript.js
+++ brainscript.js
@@ -1,2 +1,54 @@
-var element = React.createElement('div', {}, 'Hello World');
-ReactDom.render(element, document.getElementById('app'));
+// var element = React.createElement('div', {}, 'Hello World');
+
+class BrainApp extends React.Component {
+
+    state = {
+        firstName: "",
+        lastName: "",
+        date: "",
+        email: ""
+    };
+
+    onSubmit = e => {
+        e.preventDefault();
+        this.setState ({
+            firstName: "",
+            lastName: "",
+            date: "",
+            email: ""
+        });
+        this.props.onChange ({
+            firstName: "",
+            lastName: "",
+            date: "",
+            email: ""
+        });
+    };
+
+
+
+    render() {
+        return (
+            <form>
+                <div class="head"> 
+                <p>First Name:<input type="text" name="firstName" placeholder="First Name" value={this.state.firstName} onChange={e => this.change(e)} /></p>
+                <p>Last Name:<input type="text" name="lastName" placeholder="Last Name" value={this.state.lastName} onChange={e => this.change(e)} /></p>
+                <p>Event date:<input type="date" placeholder="Date" value={this.state.date} onChange={e => this.change(e)} /></p>
+                <p>Email:<input type="text" name="email" placeholder="Email" value={this.state.email} onChange={e => this.change(e)} /></p>
+                </div>
+            </form>
+        );
+    }
+}
+
+
+
+ReactDOM.render(App, document.getElementById('app'));
+
+var element = React.createElement('div', {}, 
+    React.createElement('p', {}, 'Name:'),
+    React.createElement('input', {}, )
+
+)
+
+
