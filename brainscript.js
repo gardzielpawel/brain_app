d1e62fdd7e4e8f42c1c16438c50aa2c71f746c48
Paweł Gardziel <gardzielpawel@gmail.com>
2018-07-31 00:57:11 +0200

    react w js gitignore

--- brainscript.js
+++ brainscript.js
@@ -1,25 +1,4 @@
-'use strict';
-
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
-}
-
-const domContainer = document.querySelector('#app');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('app')
+  );
