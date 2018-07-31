75328eae076c3963d88a9095c5ab652d3b907640
Paweł Gardziel <gardzielpawel@gmail.com>
2018-08-01 00:08:34 +0200

    naprawa fronta bez walidacji

--- src/Form.js
+++ src/Form.js
@@ -1,98 +1,64 @@
 import React from "react";
 
 export default class Form extends React.Component {
-  state = {
-    firstName: "",
-    lastName: "",
-    eventDate: "",
-    email: ""
-  };
-
-  change = e => {
-    this.props.onChange({ [e.target.name]: e.target.value });
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
-  onSubmit = e => {
-    e.preventDefault();
-    this.setState({
+  constructor(props) {
+    super(props);
+    this.state = {
       firstName: "",
       lastName: "",
       eventDate: "",
       email: ""
+    };
+
+    this.formChange = this.formChange.bind(this);
+    this.formSubmit = this.formSubmit.bind(this);
+  }
+  
+  formChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
     });
-    this.props.onChange({
-      firstName: "",
-      lastName: "",
-      eventDate: "",
-      email: ""
-    });
-  };
+  }
+
+  formSubmit(event) {
+    alert('A name was submitted: ' + this.state.firstName);
+    event.preventDefault();
+  }
 
   render() {
     return (
         <div className="container-fluid brain-nopadding">
-          <div className="brain-header"></div>
-            <div className="row">
+        <div className="brain-header"></div>
+          <div className="row">
+            <div className="col-md-2 brain-border"></div>
+              <div className="col-md-8 brain-onelogin">
+                  <h1 className="brain-title">Sign up for a party </h1>
+                    <form>
+                    <div className="form-group">
+                        <label htmlFor="firstName">First name</label>
+                        <input type="text" name="firstName" className="form-control" id="firstName" placeholder="First Name" value={this.state.firstName} onChange={this.formChange} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="lastName">Last name</label>
+                        <input type="text" name="lastName" className="form-control" id="lastName" placeholder="Last Name" value={this.state.lastName} onChange={this.formChange} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="eventDate">Event date</label>
+                        <input type="date" name="eventDate" className="form-control" id="eventDate" placeholder="Event Date" value={this.state.eventDate} onChange={this.formChange} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email address</label>
+                        <input type="email" name="email" className="form-control" id="email" placeholder="Email" value={this.state.email} onChange={this.formChange} />
+                    </div>
+                    <button onClick={this.formSubmit} type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
               <div className="col-md-2 brain-border"></div>
-                <div className="col-md-8 brain-onelogin">
-                    <h1 className="brain-title">Sign up for a party </h1>
-                      <form>
-                      <div className="form-group">
-                          <label htmlFor="firstName">First name</label>
-                          <input type="text" className="form-control" id="firstName" placeholder="First Name" value={this.state.firstName} onChange={e => this.change(e)} />
-                      </div>
-                      <div className="form-group">
-                          <label htmlFor="lastName">Last name</label>
-                          <input type="text" className="form-control" id="lastName" placeholder="Last Name" value={this.state.lastName} onChange={e => this.change(e)} />
-                      </div>
-                      <div className="form-group">
-                          <label htmlFor="eventDate">Event date</label>
-                          <input type="date" className="form-control" id="eventDate" placeholder="Event Date" value={this.state.eventDate} onChange={e => this.change(e)} />
-                      </div>
-                      <div className="form-group">
-                          <label htmlFor="email">Email address</label>
-                          <input type="email" className="form-control" id="email" placeholder="Email" value={this.state.email} onChange={e => this.change(e)} />
-                      </div>
-                      <button onClick={e => this.onSubmit(e)} type="submit" className="btn btn-primary">Submit</button>
-                      </form>
-                  </div>
-                <div className="col-md-2 brain-border"></div>
-            </div>
+          </div>
         </div>
-    );
+      
+    )
+
   }
 }
 
-
-/* <div className="container-fluid brain-nopadding">
-<div className="brain-header"></div>
-<div className="row">
-  <div className="col-md-2 brain-border"></div>
-  <div className="col-md-8 brain-onelogin">
-    <h1 className="brain-title">Sign up for a party </h1>
-    <form>
-      <div className="form-group">
-        <label htmlFor="firstName">First name</label>
-        <input type="text" className="form-control" id="firstName" placeholder="First Name" value={this.state.firstName} onChange={e => this.change(e)}>
-      </div>
-      <div className="form-group">
-        <label htmlFor="lastName">Last name</label>
-        <input type="text" className="form-control" id="lastName" placeholder="Last Name" value={this.state.lastName} onChange={e => this.change(e)}>
-      </div>
-      <div className="form-group">
-        <label htmlFor="eventDate">Event date</label>
-        <input type="date" className="form-control" id="eventDate" placeholder="Event Date" value={this.state.eventDate} onChange={e => this.change(e)}>
-      </div>
-      <div className="form-group">
-        <label htmlFor="email">Email address</label>
-        <input type="email" className="form-control" id="email" placeholder="Email" value={this.state.email} onChange={e => this.change(e)}>
-      </div>
-      <button onClick={e => this.onSubmit(e)} type="submit" className="btn btn-primary">Submit</button>
-    </form>
-  </div>
-  <div className="col-md-2 brain-border"></div>
-</div>
-</div> */
